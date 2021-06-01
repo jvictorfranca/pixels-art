2a0e230cc01412ef6bcad1f8348cf2b175740a56
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-06-01 17:12:33 -0300

    resolve os lints

--- script.js
+++ script.js
@@ -1,50 +1,42 @@
-let paleta = document.querySelector('div#color-palette')
-function adicionaCor(cor){
-  let botaoCor = document.createElement('div')
-  botaoCor.className = 'color'
+const paleta = document.querySelector('div#color-palette');
+function adicionaCor(cor) {
+  const botaoCor = document.createElement('div');
+  botaoCor.className = 'color';
   // botaoCor.className += cor
-  botaoCor.style.backgroundColor = cor
-  botaoCor.style.border = 'solid black 1px'
-  botaoCor.style.display = 'inline-block'
-  paleta.appendChild(botaoCor)
+  botaoCor.style.backgroundColor = cor;
+  botaoCor.style.border = 'solid black 1px';
+  botaoCor.style.display = 'inline-block';
+  paleta.appendChild(botaoCor);
 }
 
-adicionaCor('black')
-adicionaCor('red')
-adicionaCor('blue')
+adicionaCor('black');
+adicionaCor('red');
+adicionaCor('blue');
 
+const n = 5;
 
-let n = 5
+const quadro = document.querySelector('div#pixel-board');
 
-let quadro = document.querySelector('div#pixel-board')
+let tamanho = 200 / n;
+tamanho = tamanho.toString();
+tamanho += 'px';
 
-let tamanho = 200/n
-tamanho = tamanho.toString()
-tamanho += 'px'
+function criaQuadro(numero) {
+  quadro.innerHTML = null;
+  for (let index = 0; index < numero; index += 1) {
+    const linha = document.createElement('div');
+    for (let index2 = 0; index2 < numero; index2 += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.style.display = 'inline-block';
 
-function criaQuadro(n){
-  quadro.innerHTML = null
-  for (let index = 0; index < n ; index+=1 ){
-    let linha = document.createElement('div')
-    for(let index2 = 0 ; index2<n ; index2++){
-      let pixel = document.createElement('div')
-      pixel.className = 'pixel'
-      pixel.style.display = 'inline-block'
-
-      pixel.style.height = tamanho
-      pixel.style.width = tamanho
-      console.log(tamanho)
-      linha.appendChild(pixel)
+      pixel.style.height = tamanho;
+      pixel.style.width = tamanho;
+      console.log(tamanho);
+      linha.appendChild(pixel);
     }
-    linha.style.height = tamanho
-    quadro.appendChild(linha)
+    linha.style.height = tamanho;
+    quadro.appendChild(linha);
   }
 }
-criaQuadro(n)
-
-
-
-
-
-
-
+criaQuadro(n);
